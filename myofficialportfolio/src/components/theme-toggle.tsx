6c704375d17bfe267701ba2f9f01aa986730d1be
Miguel Capili <>
2026-03-09 20:23:23 +0800

    fixed the icon and name rerender

--- myofficialportfolio/src/components/theme-toggle.tsx
+++ myofficialportfolio/src/components/theme-toggle.tsx
@@ -10,37 +10,45 @@ export class ThemeToggle extends LitElement {
   `;
 
   static properties ={
-    _theme: {type: String}
+    _currentTheme: {type: String},
+    _icon: {type: String, state: true},
+    _themeName: {type: String, state: true}
   }
 
-  declare _theme: any;
+  // declare _currentTheme: any;
+  declare _currentTheme: string;
+  declare _icon: string;
+  declare _themeName: string;
+  #query = window.matchMedia('(prefers-color-scheme: dark)');
 
   constructor()
   {
     super();
-    this._theme = localStorage.getItem('theme') ?? 'light';
-    localStorage.setItem('theme', this._theme);
-    this._applyTheme(this._theme);
+    // localStorage.removeItem('theme'); 
+    this._currentTheme = localStorage.getItem('theme') ?? 'light';
+    this._applyTheme(this._currentTheme);
   }
   
-  #query = window.matchMedia('(prefers-color-scheme: dark)');
   connectedCallback() {
     super.connectedCallback();
-    
-    this.#query.addEventListener('change', (e) => {
-      if(this._theme === "system"){
-        console.log("under connected allback." + this._theme);
-        this._applyTheme("system")
-      }
-    })
+      console.log("listenniing....");
+      this.#query.addEventListener('change', this._handleSystemChange);
   }
 
+  disconnectedCallback() {
+    // Stop listening only if the Nav is removed/reset
+    // just making sure we clean up this when we swtich from main web to mobile web.
+    this.#query.removeEventListener('change', this._handleSystemChange);
+    super.disconnectedCallback();
+  }
+  
+  
   render(){
     return html`
       <wa-dropdown class="theme" @wa-select=${this._themeSelector}>
         <wa-button slot="trigger" with-caret size="small" appearance="plain" >
-          <wa-icon name="sun" variant="regular"></wa-icon> 
-          <span>Light</span>
+          <wa-icon name=${this._icon} ></wa-icon> 
+          <span>${this._themeName}</span>
         </wa-button>
         <wa-dropdown-item type="checkbox" value="light" >Light
           <wa-icon slot="icon" name="sun" variant="regular"></wa-icon>
@@ -56,21 +64,41 @@ export class ThemeToggle extends LitElement {
     `;
   }
 
+  _setIcon(theme: string) {
+    const icons = {dark: "moon", light: "sun", system: "circle-half-stroke"}
+    this._icon = icons[theme as keyof typeof icons];
+  }
+  _setName(theme: string) {
+    const icons = {dark: "Dark", light: "Light", system: "System"}
+    this._themeName = icons[theme as keyof typeof icons];
+  }
+
+  _handleSystemChange = () => {
+    if(this._currentTheme === "system"){
+        this._applyTheme("system")
+      }
+  }
+
   _themeSelector = (e: any) =>{
     localStorage.setItem('theme', e.detail.item.value);
+    console.log("_themeselector "+localStorage.getItem("theme"))
+    console.log("_themeselector e.detail "+e.detail.item.value)
     this._applyTheme(e.detail.item.value);
   }
 
   _applyTheme = (theme: string) => {
+    this._setIcon(theme);
+    this._setName(theme);
+    let isDarkMode: boolean = theme === "dark" ? true : false;
+
     if (theme==="system"){
       console.log("system sya pumasok dito");
-      theme = this.#query.matches? "dark" :"light";
+      isDarkMode = this.#query.matches? true : false;
       console.log("apply theme system "+ theme);
+      console.log("apply theme system is Darkmode"+ isDarkMode);
     }
-    document.documentElement.setAttribute('class', theme === "light" ? "wa-light":"wa-dark");
+    document.documentElement.classList.toggle("wa-dark", isDarkMode);
   }
-
-
 }
 
 customElements.define('theme-toggle', ThemeToggle);
