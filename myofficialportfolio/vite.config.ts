d1a5c807a1da9a0e66118b2a86c966752c5bee42
Miguel Paulo Capili <mpgcapili@gmail.com>
2026-04-06 02:08:49 +0800

    build fix error

--- myofficialportfolio/vite.config.ts
+++ myofficialportfolio/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/', // Ensure this is just a forward slash
   plugins: [react()],
   server:{
     host: true,
